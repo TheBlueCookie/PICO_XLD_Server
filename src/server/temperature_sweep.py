eecb3da3cbad8b83bf1e3ba0eeea306e6aa2fee6
Elias Ankerhold <elias.ankerhold@gmail.com>
2023-10-26 12:09:58 +0300

    added crashed status and test mode for temperature sweep

--- src/server/temperature_sweep.py
+++ src/server/temperature_sweep.py
@@ -7,22 +7,27 @@ from datetime import datetime
 
 
 class TemperatureSweep:
-    def __init__(self, thermalization_time, power_array, client_timeout):
+    def __init__(self, thermalization_time, power_array, client_timeout, test_mode: bool = False):
         self.thermalization_time = thermalization_time
         self.power_array = power_array
         self.db = ServerDB(db_filename)
         self.client_timeout = client_timeout
+        self.test_mode = test_mode
 
     def wait_for_all_clients(self):
         start = datetime.now()
         done = False
         while not done:
             done = True
-            for signal in self.db.get_all_meas_signals():
-                if signal == 'running' or signal == 'go':
+            for signal, crashed in self.db.get_all_meas_signals():
+                if signal == 'running' or signal == 'go' and not crashed:
                     done = False
 
             if (datetime.now() - start).total_seconds() >= self.client_timeout:
+                clients = self.db.get_html_meas_dict()
+                for meas in clients:
+                    if meas['signal'] == 'running' or meas['signal'] == 'go':
+                        self.db.set_meas_as_crashed(meas_id=meas['id'])
                 done = True
 
             sleep(5)
@@ -36,16 +41,19 @@ class TemperatureSweep:
         print(f'{datetime.now()}: Started sweep.')
 
         for i, power in enumerate(self.power_array):
-            self.db.write_heater(index=self.db.mxc_ind, val=float(power))
+            if not self.test_mode:
+                self.db.write_heater(index=self.db.mxc_ind, val=float(power))
             print(f'{datetime.now()}: Set heater power to {power} uW. '
                   f'Waiting {self.thermalization_time} s for thermalization.')
-            sleep(self.thermalization_time)
+            if not self.test_mode:
+                sleep(self.thermalization_time)
             print(
                 f'{datetime.now()}: Thermalization done. Current temperature at mixing chamber: '
                 f'{self.db.read_temp(channel=self.db.mxc_ch)} K')
             self.start_all_client_meas()
             print(f'{datetime.now()}: Send GO signal to all measurement clients.')
-            sleep(30)
+            if not self.test_mode:
+                sleep(30)
             print(
                 f'{datetime.now()}: Waiting for all measurements to finish at current '
                 f'temperature point ({i + 1}/{len(self.power_array)}).')
--- src/server/tsweep_2023-10-06.py
+++ src/server/tsweep_2023-10-06.py
@@ -4,6 +4,6 @@ import numpy as np
 
 powers = np.arange(0, 10, 5)
 
-tsweep = TemperatureSweep(thermalization_time=10, power_array=powers, client_timeout=30)
+tsweep = TemperatureSweep(thermalization_time=5, power_array=powers, client_timeout=5)
 
 tsweep.exec()
